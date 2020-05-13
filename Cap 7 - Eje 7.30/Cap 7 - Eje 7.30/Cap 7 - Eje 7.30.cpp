b13c82373dbdc1eb1297c74e469a2e7bd1f41db5
HenryAndrew356 <58161132+HenryAndrew356@users.noreply.github.com>
2020-05-13 08:44:09 -0500

    Cap 7  - Ej. 7.30 V°1

---
+++ Cap 7 - Eje 7.30/Cap 7 - Eje 7.30/Cap 7 - Eje 7.30.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<array>
+void printArray(arr[];i;f) {
+    int inicio;
+    inicio = arr[i];
+    int fin;
+    fin = arr[f];
+    if (arr.size() == 1) {
+        return arr[0];
+    }
+    else {
+
+    }
+}
+int main()
+{
+    std::cout << "Hello World!\n";
+}
